226bc8723242674398995b7cd435ec93118140aa
TiaSirio <10571322@polimi.it>
2022-09-19 23:14:43 +0200

    Update A01.m

--- A01/A01.m
+++ A01/A01.m
@@ -1,13 +1,17 @@
 clc, clear all
 
 filename = "apache1.log";
+filename2 = "apache2.log";
 
 %log = readtable(filename, 'Format', '%s%s%s%s%s%s%s%s%s%s%s')
 log = readtable(filename);
+log2 = readtable(filename2);
 
-log.Properties.VariableNames = ["IP","string1","string2","timeStamp","value1","HTTP-Request","statusCode","value2","string3","browser","serviceTimeMilliseconds"];
+finalLog = vertcat(log, log2);
 
-table = log(:,[4,11]);
+finalLog.Properties.VariableNames = ["IP","string1","string2","timeStamp","value1","HTTP-Request","statusCode","value2","string3","browser","serviceTimeMilliseconds"];
+
+table = finalLog(:,[4,11]);
 table.serviceTimeMilliseconds = milliseconds(table.serviceTimeMilliseconds);
 table.timeStamp = strrep(table.timeStamp, '[', '');
 table.timeStamp = replaceBetween(table.timeStamp,12,12,' ');
@@ -18,13 +22,9 @@ table.month = month(table.timeStampConverted);
 table.year = year(table.timeStampConverted);
 table.hour = hour(table.timeStampConverted);
 table.minute = minute(table.timeStampConverted);
-table.second = second(table.timeStampConverted)
-%table.millisecond = millisecond(table.timeStampConverted);
+%table.second = second(table.timeStampConverted);
+table.second = datetime(table.timeStamp,'InputFormat','dd/MMM/yyyy HH:mm:ss.SSS', 'Format', 'ss');
+table.millisecond = datetime(table.timeStamp,'InputFormat','dd/MMM/yyyy HH:mm:ss.SSS', 'Format', 'SSS');
+%table.millisecond = milliseconds(table.millisecond);
 
-%table = log(:,[4,11]);
-%table.serviceTimeMilliseconds = milliseconds(log.Var11);
-
-%Take the two important values needed and store it in a table
-%table.Properties.VariableNames = ["timeStamps", "serviceTimeMilliseconds"];
-
-%table
+table
