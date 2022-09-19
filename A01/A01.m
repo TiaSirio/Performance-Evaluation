4588585fff151c63552c0cfcb5690a303b4d1c4a
TiaSirio <61708227+TiaSirio@users.noreply.github.com>
2022-09-19 21:19:31 +0200

    Update A01.m

--- A01/A01.m
+++ A01/A01.m
@@ -1,10 +1,30 @@
-clear all
+clc, clear all
 
-log = readtable("apache1.log");
-table = log(:,4);
-table.millisecondsAfter = milliseconds(log.Var11);
+filename = "apache1.log";
+
+%log = readtable(filename, 'Format', '%s%s%s%s%s%s%s%s%s%s%s')
+log = readtable(filename);
+
+log.Properties.VariableNames = ["IP","string1","string2","timeStamp","value1","HTTP-Request","statusCode","value2","string3","browser","serviceTimeMilliseconds"];
+
+table = log(:,[4,11]);
+table.serviceTimeMilliseconds = milliseconds(table.serviceTimeMilliseconds);
+table.timeStamp = strrep(table.timeStamp, '[', '');
+table.timeStamp = replaceBetween(table.timeStamp,12,12,' ');
+table.timeStampConverted = datetime(table.timeStamp,'InputFormat','dd/MMM/yyyy HH:mm:ss.SSS', 'Format', 'dd/MMM/yyyy HH:mm:ss.SSS');
+
+table.day = day(table.timeStampConverted);
+table.month = month(table.timeStampConverted);
+table.year = year(table.timeStampConverted);
+table.hour = hour(table.timeStampConverted);
+table.minute = minute(table.timeStampConverted);
+table.second = second(table.timeStampConverted)
+%table.millisecond = millisecond(table.timeStampConverted);
+
+%table = log(:,[4,11]);
+%table.serviceTimeMilliseconds = milliseconds(log.Var11);
 
 %Take the two important values needed and store it in a table
-table.Properties.VariableNames = ["timeStamps", "millisecondsAfter"]
+%table.Properties.VariableNames = ["timeStamps", "serviceTimeMilliseconds"];
 
-table
+%table
