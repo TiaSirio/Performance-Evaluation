d656a2843572f9e73d166b4749ac3c6c30b07231
TiaSirio <10571322@polimi.it>
2022-09-22 00:03:52 +0200

    Update A01.m

--- A01/A01.m
+++ A01/A01.m
@@ -21,26 +21,101 @@ table.timeStampConverted = datetime(table.timeStamp,'InputFormat','dd/MMM/yyyy H
 A = size(table,1); %Arrival
 C = A; %Completions
 
-table.servedTime = table.timeStampConverted + table.serviceTimeMilliseconds;
+table.servedTime(1) = table.timeStampConverted(1) + table.serviceTimeMilliseconds(1);
 
-for i = 2:size(table)
-    table.servedTimeFixed(i) = max(table.servedTime(i - 1) + table.serviceTimeMilliseconds(i - 1), table.timeStampConverted(i));
+for i = 2 : size(table)
+    table.servedTime(i) = max(table.servedTime(i - 1), table.timeStampConverted(i)) + table.serviceTimeMilliseconds(i);
 end
 
-firstTimestamp = table.timeStampConverted(1);
-lastTimestamp = table.servedTimeFixed(A) + table.serviceTimeMilliseconds(A);
+for i = 1 : size(table) - 1
+    table.Ai(i) = seconds(table.timeStampConverted(i + 1) - table.timeStampConverted(i));
+end
 
-deltaTime = seconds(lastTimestamp - firstTimestamp);
+Ai = table.Ai; % Interarrivals times
+
+AOverdue = sum(Ai) / A; % Average interarrivals times
+
+%table
+
+firstTimestamp = table.timeStampConverted(1);
+lastTimestamp = table.servedTime(A);
+
+T = seconds(lastTimestamp - firstTimestamp);
 
 B = sum(table.serviceTimeMilliseconds); % Busy time
 
-lambda = A / deltaTime % Arrival rate
+Lambda = A / T; % Arrival rate
 
-X = C / deltaTime % Throughput
+X = C / T; % Throughput
 % Since the system is stable -> lambda = X
 
-U = B / deltaTime % Utilization
+U = B / T; % Utilization
 
 S = B / C; % Average service time
 
-Ri = table.serviceTimeMilliseconds - table.timeStampConverted
+Ri = table.serviceTimeMilliseconds; % Response times = Service times
+
+W = sum(Ri); % Workload
+
+R = W / C; % Average response time
+
+N = W / T; % Average number of jobs
+
+probabilityResponseTimeTau1 = sum(Ri > seconds(1)) / C; % Probability of having resposne time less then 1
+
+probabilityResponseTimeTau5 = sum(Ri > seconds(5)) / C; % Probability of having resposne time less then 5
+
+probabilityResponseTimeTau10 = sum(Ri > seconds(10)) / C; % Probability of having resposne time less then 10
+
+%comb = [table.timeStampConverted, ones(A, 1); table.servedTime, -ones(C, 1)]
+
+
+probabilityOfJob0 = 0;
+probabilityOfJob1 = 0;
+probabilityOfJob2 = 0;
+probabilityOfJob3 = 0;
+
+fprintf(1, "Arrival Rate:")
+Lambda
+fprintf(1, "Throughput:")
+X
+fprintf(1, "Average inter-arrival time:")
+AOverdue
+fprintf(1, "Busy time:")
+B
+fprintf(1, "Utilization:")
+U
+fprintf(1, "W:")
+W
+fprintf(1, "Average Service Time:")
+S
+fprintf(1, "Average Number of Jobs:")
+N
+fprintf(1, "Average Response Time:")
+R
+fprintf(1, "Pr(R > 1):")
+probabilityResponseTimeTau1
+fprintf(1, "Pr(R > 5):")
+probabilityResponseTimeTau5
+fprintf(1, "Pr(R > 10):")
+probabilityResponseTimeTau10
+fprintf(1, "Pr(N = 0):")
+probabilityOfJob0
+fprintf(1, "Pr(N = 1):")
+probabilityOfJob1
+fprintf(1, "Pr(N = 2):")
+probabilityOfJob2
+fprintf(1, "Pr(N = 2):")
+probabilityOfJob3
+
+%fprintf(1, "Arrival Rate: %g, Throughput %g\n", Lambda, X);
+%fprintf(1, "Average inter-arrival time: %g\n", AOverdue);
+%fprintf(1, "Busy time: %s\n", B);
+%fprintf(1, "Utilization: %d\n", U);
+%fprintf(1, "W: %g\n", W);
+%fprintf(1, "Average Service Time: %g\n", S);
+%fprintf(1, "Average Number of Jobs: %g\n", N);
+%fprintf(1, "Average Response Time: %g\n", R);
+%fprintf(1, "Pr(R > 1): %g\n", probabilityResponseTimeTau1);
+%fprintf(1, "Pr(R > 5): %g\n", probabilityResponseTimeTau5);
+%fprintf(1, "Pr(R > 10): %g\n", probabilityResponseTimeTau10);
